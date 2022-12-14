7aa790a0a636134aa6d86ea470a92e5573bfb4a3
Mia <211111037@mhs.stiki.ac.id>
2022-12-14 16:01:07 +0700

    menambah tampilan detail buku

--- app/Views/mahasiswa/detailbuku.php
+++ app/Views/mahasiswa/detailbuku.php
@@ -1,1 +1,90 @@
-<h1>Ini detail buku</h1>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Shop Item - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link rel="stylesheet" href="<?= base_url('css/styles.css') ?>">
+    </head>
+    <body>
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#!">PERPUS STIKI</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Home</a><ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#!">All Products</a></li>
+                                <li><hr class="dropdown-divider" /></li>
+                                <li><a class="dropdown-item" href="#!">Popular Items</a></li>
+                                <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
+                            </ul></li>
+                        <li class="nav-item dropdown"><a a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">History</a><ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#!">All Products</a></li>
+                                <li><hr class="dropdown-divider" /></li>
+                                <li><a class="dropdown-item" href="#!">Popular Items</a></li>
+                                <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
+                            </ul></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Event</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">About</a></li>
+                    </ul>
+                    <form class="d-flex">
+                        
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <!-- Product section-->
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="center">
+                    <img class="card-img-top" src="image 1.png" alt="..." />
+                </div>
+                <div class="center">
+                    
+                    <div class="">
+                        
+                        <h1 class="display-5 fw-bolder"> = Informasi Buku = </h1>
+                        <div class="fs-5 mb-5">
+                            <span>Judul Buku : Cantik Itu Luka</span> <br>
+                            <span>Penulis : Eka Kurniawan </span> <br>
+                            <span>Penerbit : Gramedia</span> <br> <br> <br>
+
+                            <span class="justify-content-center">Sinopsis : </span>
+                        </div>
+                        <p class="justify-content-center">Novel Cantik Itu Luka berfokus pada kisah hidup Dewi Ayu, seorang pelacur di zaman kolonial.. Suatu sore di Halimunda, Dewi Ayu bangkit dari kuburannya setealh dua puluh satu tahun kematainnya. Kebangkitanya ini jelas menimbulkan kegaduhan bagi masyarakat. Dewi Ayu adalah sroang wanita yang meninggal ketika berusia 51 tahun. Sebelum meninggal, ia adalah seorang pelacur yang sanagt terpndnag di kalangan para tentara Jepang dan Belanda. Ia sendiri memiliki kisah hidup asa kecilnya juga tak kalah pedih. Sejak masih kecil, ia tumbuh tanpa kasih sayang kedua orang tuanya akibat perkawinan sedarah (perkawinan saudara tiri) yang membuat mereka berdua diusir. Alhasil, Ia pun diasuh oleh kakek-neneknya.Ia pun tumbuh menjadi gadis pemberani. Keberaniannya ini telah ia buktikan tatkala ia harus mendekam di penjara saat Jepang menyerang Hindia Belanda. Saat berada di penjara, Dewi Ayu bahkan rela menyerahkan kesucianya demi membantu temanya di barak penampungan. Dua tahun kemudian, Dewi Ayu bersama 19 tahanan lainnya dipindahkan ke rumah mewah yyang dikelola oleh Mama Kalong untuk bekerja sebagai pelacur. Karena pekerjaannya itu, Ia pun harus melayani nafsu para tentara Jepang.Selama ia menjadi pelacur, Dewi Ayu telah melahirkan empat anak perempuan yang tidak jelas asal-usul ayahnya. Ketiga anaknya mewarisi wajah cantik dirinya. Tapi, ia merasa mengaush tiga anak cantik itu sangat merepotkan.</p>
+                        <div class="d-flex">
+                            <button class="btn btn-outline-dark flex-shrink-0" type="button">
+                              
+                                Reservasi
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Related items section-->
+        <section class="py-5 bg-light">
+            <div class="container px-4 px-lg-5 mt-5">
+                <h2 class="fw-bolder mb-4">Related products</h2>
+                
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
