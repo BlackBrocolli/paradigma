8872334569a4d4576d305eb38010712d1b17e279
Mia <211111037@mhs.stiki.ac.id>
2022-12-20 21:40:23 +0700

    update tampilan detail buku

--- app/Views/mahasiswa/detailbuku.php
+++ app/Views/mahasiswa/detailbuku.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Shop Item - Start Bootstrap Template</title>
+        <title>Detail Buku</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Bootstrap icons-->
@@ -19,6 +19,7 @@
             <div class="container px-4 px-lg-5">
                 <a class="navbar-brand" href="#!">PERPUS STIKI</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                         <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Home</a><ul class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -37,6 +38,13 @@
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">About</a></li>
                     </ul>
                     <form class="d-flex">
+                        <button class="btn btn-outline-dark" type="submit">
+                            <i class="bi-person-fill me-1"></i>
+                            Dewi Sartika
+                            <!-- <span class="badge bg-dark text-white ms-1 rounded-pill">0</span> -->
+                        </button>
+                    </form>
+                    <form class="d-flex">
                         
                     </form>
                 </div>
@@ -45,28 +53,42 @@
         <!-- Product section-->
         <section class="py-5">
             <div class="container px-4 px-lg-5 my-5">
+                <div class="form-group has-search">
+                <span class="fa fa-search form-control-feedback"></span>
+                    <input type="text" class="form-control" placeholder="Cari">
+                 </div><br><br><br>
+
                 <div class="center">
-                    <img class="card-img-top" src="image 1.png" alt="..." />
+                    <img class="mx-auto d-block" src="https://th.bing.com/th/id/R.5933333ffe2cbfbfef0b4d9a8c9873cf?rik=6VNlXFPdGnLRUw&riu=http%3a%2f%2fdimensipers.com%2fwp-content%2fuploads%2f2020%2f04%2fimages-24.jpeg&ehk=AkmKMkfequUKBfhTAqaooVDrLR4VUT4yhhQ9hTAuqCE%3d&risl=&pid=ImgRaw&r=0" alt="..." />
                 </div>
                 <div class="center">
                     
                     <div class="">
-                        
-                        <h1 class="display-5 fw-bolder"> = Informasi Buku = </h1>
-                        <div class="fs-5 mb-5">
-                            <span>Judul Buku : Cantik Itu Luka</span> <br>
-                            <span>Penulis : Eka Kurniawan </span> <br>
-                            <span>Penerbit : Gramedia</span> <br> <br> <br>
 
-                            <span class="justify-content-center">Sinopsis : </span>
+                        
+                        
+                        <div class="fs-5 mb-5">
+                            <figure class="text-center">
+                             <blockquote class="blockquote">
+                                    <p><h1 class="display-5 fw-bolder"> = Informasi Buku = </h1>
+                                        <span>Judul Buku : Cantik Itu Luka</span> <br>
+                                        <span>Penulis : Eka Kurniawan </span> <br>
+                                        <span>Penerbit : Gramedia</span> <br> <br> <br>
+                                        <span >Sinopsis : </span></p>
+                             </blockquote>
+                            </figure>
+                            
+
+                            
                         </div>
+                        <figure class="text-center">
+                             
                         <p class="justify-content-center">Novel Cantik Itu Luka berfokus pada kisah hidup Dewi Ayu, seorang pelacur di zaman kolonial.. Suatu sore di Halimunda, Dewi Ayu bangkit dari kuburannya setealh dua puluh satu tahun kematainnya. Kebangkitanya ini jelas menimbulkan kegaduhan bagi masyarakat. Dewi Ayu adalah sroang wanita yang meninggal ketika berusia 51 tahun. Sebelum meninggal, ia adalah seorang pelacur yang sanagt terpndnag di kalangan para tentara Jepang dan Belanda. Ia sendiri memiliki kisah hidup asa kecilnya juga tak kalah pedih. Sejak masih kecil, ia tumbuh tanpa kasih sayang kedua orang tuanya akibat perkawinan sedarah (perkawinan saudara tiri) yang membuat mereka berdua diusir. Alhasil, Ia pun diasuh oleh kakek-neneknya.Ia pun tumbuh menjadi gadis pemberani. Keberaniannya ini telah ia buktikan tatkala ia harus mendekam di penjara saat Jepang menyerang Hindia Belanda. Saat berada di penjara, Dewi Ayu bahkan rela menyerahkan kesucianya demi membantu temanya di barak penampungan. Dua tahun kemudian, Dewi Ayu bersama 19 tahanan lainnya dipindahkan ke rumah mewah yyang dikelola oleh Mama Kalong untuk bekerja sebagai pelacur. Karena pekerjaannya itu, Ia pun harus melayani nafsu para tentara Jepang.Selama ia menjadi pelacur, Dewi Ayu telah melahirkan empat anak perempuan yang tidak jelas asal-usul ayahnya. Ketiga anaknya mewarisi wajah cantik dirinya. Tapi, ia merasa mengaush tiga anak cantik itu sangat merepotkan.</p>
-                        <div class="d-flex">
-                            <button class="btn btn-outline-dark flex-shrink-0" type="button">
-                              
-                                Reservasi
-                            </button>
-                        </div>
+
+                            </figure>
+                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Reservasi</a></div>
+                            </div>
                     </div>
                 </div>
             </div>
@@ -74,7 +96,19 @@
         <!-- Related items section-->
         <section class="py-5 bg-light">
             <div class="container px-4 px-lg-5 mt-5">
-                <h2 class="fw-bolder mb-4">Related products</h2>
+                <nav aria-label="Page navigation example">
+  <ul class="pagination pg-blue justify-content-end">
+    <li class="page-item disabled">
+      <a class="page-link" tabindex="-1">Previous</a>
+    </li>
+    <li class="page-item"><a class="page-link">1</a></li>
+    <li class="page-item"><a class="page-link">2</a></li>
+    <li class="page-item"><a class="page-link">3</a></li>
+    <li class="page-item">
+      <a class="page-link">Next</a>
+    </li>
+  </ul>
+</nav>
                 
             </div>
         </section>
