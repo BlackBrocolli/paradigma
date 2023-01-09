706c8649bda8cd49c026cfef0e19c6df0253f51f
BlackBrocolli <201111011@mhs.stiki.ac.id>
2023-01-09 22:33:06 +0700

    Menyelesaikan reservasi bagian mahasiswa

--- app/Views/mahasiswa/templates/index.php
+++ app/Views/mahasiswa/templates/index.php
@@ -105,7 +105,7 @@
     <!-- Footer-->
     <footer class="py-5 bg-dark">
         <div class="container">
-            <p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p>
+            <p class="m-0 text-center text-white">Copyright &copy; Paradigma Pemrograman 2022/2023</p>
         </div>
     </footer>
 
