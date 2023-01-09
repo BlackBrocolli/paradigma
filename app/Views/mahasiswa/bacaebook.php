68f0ca1c129abc73ed69b07eeb0cd36505143564
Mia <211111037@mhs.stiki.ac.id>
2023-01-09 20:04:53 +0700

    menambahkan tampilan baca ebook

--- app/Views/mahasiswa/bacaebook.php
+++ app/Views/mahasiswa/bacaebook.php
@@ -3,9 +3,10 @@
 <?= $this->section('page-content'); ?>
 
 <!-- Baca ebook edit disini -->
-
-Edit baca ebook
-
+    <h4>Judul Buku : </h4> <br><br><br>
+    <center>
+        <embed type="application/pdf" src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210101201653/PDF.pdf#toolbar=0" width="1200" height="900"></embed>
+    </center>
 <!-- akhir baca ebook -->
 
 <?= $this->endSection(); ?>
